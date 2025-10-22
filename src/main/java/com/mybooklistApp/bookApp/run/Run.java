b9f9012bd41969a9121168856cf99d7446bdb44b
Sohaib Ebrahimi <ebrahimi_sa@icloud.com>
2025-10-22 10:26:38 +0300

    cruds done

--- src/main/java/com/mybooklistApp/bookApp/run/Run.java
+++ src/main/java/com/mybooklistApp/bookApp/run/Run.java
@@ -4,11 +4,22 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run(
     Integer id,
+    @NotEmpty
     String title,
     LocalDateTime started0n,
     LocalDateTime completed0n,
+    @Positive
     Integer miles,
     Location Location
-) {}
+) {
+    public Run {
+        if(!completed0n.isAfter(started0n)){
+            throw new IllegalArgumentException("completed must be after started");
+        }
+    }
+}
